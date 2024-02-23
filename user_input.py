df81f01b055ee4c7cab5a73ea31fcebb3a6ec0ac
maruusik <isaackipruto84@gmail.com>
2024-02-23 23:13:01 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,18 @@
+# user_input.py
+
+# Ask user for their name
+name = input("What is your name? ")
+
+# Ask user for their age
+age = input("How old are you? ")
+
+# Ask user for their location
+location = input("Where are you located? ")
+
+name = str("Brielle")
+age = int(7)
+location =str("Eldoret")
+
+# Print personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+
